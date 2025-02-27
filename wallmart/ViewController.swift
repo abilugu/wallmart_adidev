f63bb703950da5c9c95ed0481639365f9d07cbac
Aravind Bilugu <abilugu@gmail.com>
2025-02-27 11:47:53 -0600

    Initial Commit

---
+++ wallmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wallmart
+//
+//  Created by Aravind Bilugu on 2/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ wallmartTests/wallmartTests.swift
@@ -0,0 +1,17 @@
+//
+//  wallmartTests.swift
+//  wallmartTests
+//
+//  Created by Aravind Bilugu on 2/27/25.
+//
+
+import Testing
+@testable import wallmart
+
+struct wallmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
